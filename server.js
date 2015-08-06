d88d747458bee88149ba1dd60589b58e2c56a6b7
vikram ravichandran <vikram06@ymail.com>
2015-08-06 00:32:21 -0700

    committed README

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/view'));
+
+app.listen(process.env.PORT || 3000);
+
+console.log('-------------------------------------------------------------------');
+console.log('Application available at http://localhost:' + (process.env.PORT || 3000))
+console.log('-------------------------------------------------------------------');
